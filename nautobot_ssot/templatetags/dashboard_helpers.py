39a56000dfd47e2d5e03b68683fa2bc0fba62493
Glenn Matthews <glenn.matthews@networktocode.com>
2021-06-28 12:58:07 -0400

    Fix incorrect filtering of data in dashboard view

--- nautobot_ssot/templatetags/dashboard_helpers.py
+++ nautobot_ssot/templatetags/dashboard_helpers.py
@@ -15,7 +15,7 @@ register = template.Library()
 def dashboard_data(sync_worker_class, queryset, kind="source"):
     """Render data about the sync history of a specific data-source or data-target."""
     if kind == "source":
-        records = queryset.filter(source=sync_worker_class.name).order_by("-start_time")
+        records = queryset.filter(source=sync_worker_class.data_source).order_by("-start_time")
     else:
-        records = queryset.filter(target=sync_worker_class.name).order_by("-start_time")
+        records = queryset.filter(target=sync_worker_class.data_target).order_by("-start_time")
     return {"syncs": records[:10], "count": records.count()}
