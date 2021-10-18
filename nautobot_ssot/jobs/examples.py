e5aebfe280deef209d15b42f43a4b7d096cf1bce
Uros Bajzelj <uros@bajzelj.com>
2021-10-18 19:27:08 +0200

    Add the name for the section in Nautobot jobs

--- nautobot_ssot/jobs/examples.py
+++ nautobot_ssot/jobs/examples.py
@@ -18,6 +18,8 @@ from nautobot_ssot.jobs.base import DataMapping, DataSource, DataTarget
 
 # In a more complex Job, you would probably want to move the DiffSyncModel subclasses into a separate Python module(s).
 
+name = "SSoT Examples" # pylint: disable=invalid-name
+
 
 class RegionModel(DiffSyncModel):
     """Shared data model representing a Region in either of the local or remote Nautobot instances."""
