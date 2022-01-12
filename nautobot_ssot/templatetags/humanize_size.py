91f13d05b3fb650f5e85c04be61a8f5ca595ee5e
Christian Adell <chadell@gmail.com>
2022-01-12 06:49:16 +0100

    Add humanize_size template filter
    fix humanize

---
+++ nautobot_ssot/templatetags/humanize_size.py
@@ -0,0 +1,23 @@
+"""Simple template tag to render a shorter representation of a timedelta object."""
+
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter()
+def humanize_size(size):
+    """Humanize memory size given in bytes.
+
+    Examples:
+        1 => "1.00 B"
+        1024 => "1.00 KiB"
+        1024*1024 => "1.00 MiB"
+    """
+    suffix = "B"
+    for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+        if abs(size) < 1024.0:
+            return f"{size:3.2f} {unit}{suffix}"
+        size /= 1024.0
+    return f"{size:.1f} Yi{suffix}"
