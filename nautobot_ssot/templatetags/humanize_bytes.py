777a72399571e07f1265c95228c4a71d38943e65
Christian Adell <chadell@gmail.com>
2022-01-12 17:47:39 +0100

    Tune humanize_bytes to skip 0.00 decimals

--- nautobot_ssot/templatetags/humanize_bytes.py
+++ nautobot_ssot/templatetags/humanize_bytes.py
@@ -11,13 +11,16 @@ def humanize_bytes(size):
     """Humanize memory size given in bytes.
 
     Examples:
-        1 => "1.00 B"
-        1024 => "1.00 KiB"
-        1024*1024 => "1.00 MiB"
+        1 => "1 B"
+        1024 => "1 KiB"
+        1024*1024 => "1 MiB"
     """
     suffix = "B"
     for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
         if abs(size) < 1024.0:
+            if round(size, 2) % 1 < 0.01:
+                # If there are no relevant decimals "0.00" we skip them
+                return f"{size:3.0f} {unit}{suffix}"
             return f"{size:3.2f} {unit}{suffix}"
         size /= 1024.0
     return f"{size:.1f} Yi{suffix}"
