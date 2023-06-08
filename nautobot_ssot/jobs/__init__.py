86c14f283e6e9d6058dfb1336e1962e6bd7d24a3
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-06-08 08:21:41 -0500

    fix: Temporarily disable get_data_jobs functionality as get_jobs() function was removed from core.

--- nautobot_ssot/jobs/__init__.py
+++ nautobot_ssot/jobs/__init__.py
@@ -2,12 +2,7 @@
 
 from django.conf import settings
 
-from nautobot.extras.jobs import get_jobs
-
-from nautobot_ssot.integrations.utils import each_enabled_integration_module
-from nautobot_ssot.utils import logger
-
-from .base import DataSource, DataTarget
+# from .base import DataSource, DataTarget
 from .examples import ExampleDataSource, ExampleDataTarget
 
 if settings.PLUGINS_CONFIG["nautobot_ssot"]["hide_example_jobs"]:
@@ -30,15 +25,15 @@ _add_integrations()
 
 def get_data_jobs():
     """Get all data-source and data-target jobs available."""
-    jobs_dict = get_jobs()
+    # jobs_dict = get_jobs()
     data_sources = []
     data_targets = []
-    for modules in jobs_dict.values():
-        for module_data in modules.values():
-            for job_class in module_data["jobs"].values():
-                if issubclass(job_class, DataSource):
-                    data_sources.append(job_class)
-                if issubclass(job_class, DataTarget):
-                    data_targets.append(job_class)
+    # for modules in jobs_dict.values():
+    #     for module_data in modules.values():
+    #         for job_class in module_data["jobs"].values():
+    #             if issubclass(job_class, DataSource):
+    #                 data_sources.append(job_class)
+    #             if issubclass(job_class, DataTarget):
+    #                 data_targets.append(job_class)
 
     return (data_sources, data_targets)
