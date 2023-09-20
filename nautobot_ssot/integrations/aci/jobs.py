27853eff24860fbdea942d27c282b2e6fa3073d8
Christian Adell <chadell@gmail.com>
2023-09-20 17:19:44 +0200

    ix pylint

--- nautobot_ssot/integrations/aci/jobs.py
+++ nautobot_ssot/integrations/aci/jobs.py
@@ -51,7 +51,7 @@ class AciDataSource(DataSource, Job):  # pylint: disable=abstract-method
     def __init__(self):
         """Initialize ExampleYAMLDataSource."""
         super().__init__()
-        self.diffsync_flags = self.diffsync_flags | DiffSyncFlags.SKIP_UNMATCHED_DST
+        self.diffsync_flags = self.diffsync_flags | DiffSyncFlags.SKIP_UNMATCHED_DST # pylint: disable=unsupported-binary-operation
 
     @classmethod
     def data_mappings(cls):
