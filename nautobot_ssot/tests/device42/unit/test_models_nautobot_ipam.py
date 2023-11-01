70a651533e7a5c41ab6e7cd09d8d1cd897d41e16
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-11-01 15:30:10 -0500

    test: ✅ Add initial test for VRFGroup class

---
+++ nautobot_ssot/tests/device42/unit/test_models_nautobot_ipam.py
@@ -0,0 +1,31 @@
+"""Test DiffSync IPAM models for Nautobot."""
+from unittest.mock import MagicMock
+from diffsync import DiffSync
+from nautobot.core.testing import TransactionTestCase
+from nautobot.ipam.models import Namespace, VRF
+from nautobot_ssot.integrations.device42.diffsync.models.nautobot import ipam
+
+
+class TestNautobotVRFGroup(TransactionTestCase):
+    """Test the NautobotVRFGroup class."""
+
+    def setUp(self):
+        self.diffsync = DiffSync()
+        self.diffsync.namespace_map = {}
+        self.diffsync.vrf_map = {}
+        self.diffsync.job = MagicMock()
+        self.diffsync.job.logger.info = MagicMock()
+
+    def test_create(self):
+        """Validate the NautobotVRFGroup create() method creates a VRF."""
+        ids = {"name": "Test"}
+        attrs = {"description": "Test VRF", "tags": ["test"], "custom_fields": {}}
+        result = ipam.NautobotVRFGroup.create(self.diffsync, ids, attrs)
+        self.assertIsInstance(result, ipam.NautobotVRFGroup)
+        self.diffsync.job.logger.info.assert_called_once_with("Creating VRF Test.")
+        namespace = Namespace.objects.get(name=ids["name"])
+        self.assertEqual(namespace.name, ids["name"])
+        self.assertEqual(self.diffsync.namespace_map[ids["name"]], namespace.id)
+        vrf = VRF.objects.get(name=ids["name"])
+        self.assertEqual(self.diffsync.vrf_map[ids["name"]], vrf.id)
+        self.assertEqual(vrf.namespace.name, ids["name"])
