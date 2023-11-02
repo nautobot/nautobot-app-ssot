4555eb9bcba2713c38e7ac8ab03a9068b832ea81
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-11-02 09:02:03 -0500

    test: ✅ Add test validating VRFGroup.update()

--- nautobot_ssot/tests/device42/unit/test_models_nautobot_ipam.py
+++ nautobot_ssot/tests/device42/unit/test_models_nautobot_ipam.py
@@ -29,3 +29,20 @@ class TestNautobotVRFGroup(TransactionTestCase):
         vrf = VRF.objects.get(name=ids["name"])
         self.assertEqual(self.diffsync.vrf_map[ids["name"]], vrf.id)
         self.assertEqual(vrf.namespace.name, ids["name"])
+
+    def test_update(self):
+        """Validate the NautobotVRFGroup update() updates a VRF."""
+        vrf = VRF.objects.create(name="Test")
+        vrf.validated_save()
+        test_vrf = ipam.NautobotVRFGroup(
+            name="Test", description="Test VRF", tags=["test"], custom_fields={}, uuid=vrf.id
+        )
+        test_vrf.diffsync = MagicMock()
+        test_vrf.diffsync.job.logger.info = MagicMock()
+        update_attrs = {"description": "Test VRF Update", "custom_fields": {"test": {"key": "test", "value": "test"}}}
+        actual = ipam.NautobotVRFGroup.update(self=test_vrf, attrs=update_attrs)
+        test_vrf.diffsync.job.logger.info.assert_called_once_with("Updating VRF Test.")
+        vrf.refresh_from_db()
+        self.assertEqual(vrf.description, update_attrs["description"])
+        self.assertEqual(vrf.custom_field_data["test"], "test")
+        self.assertEqual(actual, test_vrf)
