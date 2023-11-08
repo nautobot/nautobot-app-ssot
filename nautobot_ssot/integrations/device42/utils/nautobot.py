353ee51aaf919b442e414e9d18c04cb58f0d69de
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-11-08 11:11:39 -0600

    style: 🔥 Remove unused import

--- nautobot_ssot/integrations/device42/utils/nautobot.py
+++ nautobot_ssot/integrations/device42/utils/nautobot.py
@@ -12,7 +12,6 @@ from nautobot.circuits.models import CircuitType
 from nautobot.dcim.models import Device, Interface, Platform
 from nautobot.extras.choices import CustomFieldTypeChoices
 from nautobot.extras.models import CustomField, Relationship, Role, Tag
-from nautobot.ipam.models import IPAddress
 from netutils.lib_mapper import ANSIBLE_LIB_MAPPER_REVERSE, NAPALM_LIB_MAPPER_REVERSE
 from taggit.managers import TaggableManager
 from nautobot_ssot.integrations.device42.diffsync.models.base.dcim import Device as NautobotDevice
