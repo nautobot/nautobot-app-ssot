3a5dbcafea88e981d1fb5c166ed511c77e51e7f4
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2023-11-16 09:36:49 -0600

    refactor: Update migration to avoid losing CustomField data.

--- nautobot_ssot/migrations/0007_replace_dashed_custom_fields.py
+++ nautobot_ssot/migrations/0007_replace_dashed_custom_fields.py
@@ -13,10 +13,10 @@ def replace_dashed_custom_fields(apps, schema_editor):
     CustomField = apps.get_model("extras", "customfield")
     ContentType = apps.get_model("contenttypes", "ContentType")
 
-    for key in CF_KEY_CHANGE_MAP.values():
-        for custom_field in CustomField.objects.filter(key=key):
+    for new_key, old_key in CF_KEY_CHANGE_MAP.items():
+        for custom_field in CustomField.objects.filter(key=old_key):
             original_key = custom_field.key
-            updated_key = CF_KEY_CHANGE_MAP[key]
+            updated_key = CF_KEY_CHANGE_MAP[new_key]
             print(
                 f'CustomField instance "{custom_field.label}" key attribute "{original_key}" is being changed to "{updated_key}".'
             )
@@ -30,9 +30,10 @@ def replace_dashed_custom_fields(apps, schema_editor):
         model = apps.get_model(ct.app_label, ct.model)
         cf_list = []
         for instance in model.objects.all():
-            new_custom_field_data = {}
+            new_custom_field_data = instance._custom_field_data.copy()
             for cf in relevant_custom_fields:
-                new_custom_field_data[cf.key] = instance._custom_field_data.pop(CF_KEY_CHANGE_MAP.get(cf.key), None)
+                if cf.key not in new_custom_field_data:
+                    new_custom_field_data[cf.key] = instance._custom_field_data.pop(CF_KEY_CHANGE_MAP.get(cf.key), None)
             instance._custom_field_data = new_custom_field_data
             cf_list.append(instance)
         model.objects.bulk_update(cf_list, ["_custom_field_data"], 1000)
