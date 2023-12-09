2f33fef44de2e0dc34497c4eff93c282e161bce4
Gerardo Castaldo <48739169+gerardocastaldo@users.noreply.github.com>
2023-12-09 14:33:38 +0100

    Update cloudvision.py

--- nautobot_ssot/integrations/aristacv/diffsync/adapters/cloudvision.py
+++ nautobot_ssot/integrations/aristacv/diffsync/adapters/cloudvision.py
@@ -129,14 +129,12 @@ class CloudvisionAdapter(DiffSync):
                     self.add(new_port)
                     device.add_child(new_port)
                     if self.job.debug:
-                        self.job.logger.debug(f"
-                            Added {port['interface']} for {device.name}. 
+                        self.job.logger.debug(f"""Added {port['interface']} for {device.name}. 
                             description: {port_description}
                             enabled: {port['enabled']}
                             status: {port_status]
                             port_type: {port_type},
-                            mode: {port_mode}
-                        ")
+                            mode: {port_mode}""")
                 except ObjectAlreadyExists as err:
                     self.job.logger.warning(
                         f"Duplicate port {port['interface']} found for {device.name} and ignored. {err}"
