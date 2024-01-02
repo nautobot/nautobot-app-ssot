9da16fff28908690c4b9abb2ffd66b7fccdccb07
Jacob McGill <jacob.mcgill@networktocode.com>
2024-01-02 17:22:58 -0500

    Add support for filtering by custom_field SoR

--- nautobot_ssot/management/commands/elongate_interface_names.py
+++ nautobot_ssot/management/commands/elongate_interface_names.py
@@ -29,15 +29,32 @@ class Command(BaseCommand):
             help="DCIM.Location names to limit which devices have their interface names updated (comma separated).",
         )
 
+        parser.add_argument(
+            "--cf_systems_of_record",
+            default=None,
+            help="Limits DCIM.Devices in scope to those that have a custom_field.system_of_record value to what is passed.",
+        )
+
     def handle(self, *args, **options):  # noqa: D102
-        if options.get("devices"):
-            device_option = [device.strip() for device in options["devices"].split(",")]
+        device_limit = options.get("devices")
+        location_limit = options.get("locations")
+        if device_limit and location_limit:
+            raise ValueError('Only one of "--devices" and "--locations" may be used.')
+
+        if device_limit:
+            device_option = [device.strip() for device in device_limit.split(",")]
             devices = Device.objects.filter(name__in=device_option)
-        elif options.get("locations"):
-            device_option = [location.strip() for location in options["locations"].split(",")]
+        elif location_limit:
+            device_option = [location.strip() for location in location_limit.split(",")]
             devices = Device.objects.filter(location__name__in=device_option)
         else:
             devices = Device.objects.all()
+
+        cf_systems_of_record_limit = options.get("cf_systems_of_record")
+        if cf_systems_of_record_limit:
+            sor_option = [sor.strip() for sor in cf_systems_of_record_limit.split(",")]
+            devices = devices.filter(_custom_field_data__system_of_record__in=sor_option)
+
         for device in devices:
             for interface in device.interfaces.all():
                 new_name = canonical_interface_name(interface.name)
--- nautobot_ssot/tests/test_management.py
+++ nautobot_ssot/tests/test_management.py
@@ -13,16 +13,19 @@ from nautobot.dcim.models import (
     Manufacturer,
     Platform,
 )
-from nautobot.extras.models import Role, Status
+from nautobot.extras.models import CustomField, Role, Status
 
 
 class TestElongateInterfaceNames(TestCase):
     """Unittests for elongate_interface_names command."""
 
-    def setUp(self):
+    def setUp(self):  # pylint: disable=too-many-locals
         """Per-test setup."""
         ct_device = ContentType.objects.get_for_model(Device)
         active_status = Status.objects.get(name="Active")
+
+        cf_sor = CustomField.objects.create(label="system_of_record", type="text")
+        cf_sor.content_types.add(ct_device)
 
         role = Role.objects.create(name="ssot_test")
         role.content_types.add(ct_device)
@@ -43,6 +46,7 @@ class TestElongateInterfaceNames(TestCase):
             platform=platform,
             role=role,
             device_type=device_type,
+            _custom_field_data={"system_of_record": "ipfabric"},
         )
         device2 = Device.objects.create(
             name="ssot_test_2",
@@ -51,6 +55,7 @@ class TestElongateInterfaceNames(TestCase):
             platform=platform,
             role=role,
             device_type=device_type,
+            _custom_field_data={"system_of_record": "servicenow"},
         )
         device3 = Device.objects.create(
             name="ssot_test_3",
@@ -137,3 +142,48 @@ class TestElongateInterfaceNames(TestCase):
                 "Updating ssot_test_4.ge2 >> GigabitEthernet2"
             ),
         )
+
+    def test_pass_devices_and_locations_arg(self):
+        out = StringIO()
+        with self.assertRaises(ValueError, msg='Only one of "--devices" and "--locations" may be used.'):
+            call_command(
+                "elongate_interface_names",
+                "--no-color",
+                "--skip-checks",
+                devices="ssot_test_1",
+                locations="ssot_test_1",
+                stdout=out,
+            )
+
+    def test_pass_cf_sor_arg_single(self):
+        out = StringIO()
+        call_command(
+            "elongate_interface_names", "--no-color", "--skip-checks", cf_systems_of_record="ipfabric", stdout=out
+        )
+        self.assertEqual(out.getvalue().strip(), "Updating ssot_test_1.ge2 >> GigabitEthernet2")
+
+    def test_pass_cf_sor_arg_multiple(self):
+        out = StringIO()
+        call_command(
+            "elongate_interface_names",
+            "--no-color",
+            "--skip-checks",
+            cf_systems_of_record="ipfabric,servicenow",
+            stdout=out,
+        )
+        self.assertEqual(
+            out.getvalue().strip(),
+            "Updating ssot_test_1.ge2 >> GigabitEthernet2\nUpdating ssot_test_2.ge2 >> GigabitEthernet2",
+        )
+
+    def test_pass_location_and_cf_sor_args(self):
+        out = StringIO()
+        call_command(
+            "elongate_interface_names",
+            "--no-color",
+            "--skip-checks",
+            locations="ssot_test_1,ssot_test_3",
+            cf_systems_of_record="ipfabric",
+            stdout=out,
+        )
+        self.assertEqual(out.getvalue().strip(), "Updating ssot_test_1.ge2 >> GigabitEthernet2")
