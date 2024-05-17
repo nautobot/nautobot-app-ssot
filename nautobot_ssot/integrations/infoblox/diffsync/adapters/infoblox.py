0f59c7d18fe943f6f51ad2e03a6749d347ec2c89
Przemek Rogala <progala@progala.net>
2024-05-17 20:25:17 +0100

    - Load host/a/ptr records related to IPs. - Add custom exception raised when loading fails. - Fix prefix/ip address filtering bug. - Get config from the SSOTInfobloxConfig object. - Add excluded attributes logic.

--- nautobot_ssot/integrations/infoblox/diffsync/adapters/infoblox.py
+++ nautobot_ssot/integrations/infoblox/diffsync/adapters/infoblox.py
@@ -3,12 +3,13 @@
 import re
 from typing import Optional
 
+import requests
+
 from diffsync import DiffSync
 from diffsync.enum import DiffSyncFlags
 from diffsync.exceptions import ObjectAlreadyExists
 from nautobot.extras.plugins.exceptions import PluginImproperlyConfigured
 
-from nautobot_ssot.integrations.infoblox.constant import PLUGIN_CFG
 from nautobot_ssot.integrations.infoblox.diffsync.models.infoblox import (
     InfobloxIPAddress,
     InfobloxNamespace,
@@ -24,6 +25,10 @@ from nautobot_ssot.integrations.infoblox.utils.diffsync import (
 )
 
 
+class AdapterLoadException(Exception):
+    """Raised when there's an error while loading data."""
+
+
 class InfobloxAdapter(DiffSync):
     """DiffSync adapter using requests to communicate to Infoblox server."""
 
@@ -35,18 +40,21 @@ class InfobloxAdapter(DiffSync):
 
     top_level = ["namespace", "vlangroup", "vlan", "prefix", "ipaddress"]
 
-    def __init__(self, *args, job=None, sync=None, conn, **kwargs):
+    def __init__(self, *args, job=None, sync=None, conn, config, **kwargs):
         """Initialize Infoblox.
 
         Args:
             job (object, optional): Infoblox job. Defaults to None.
             sync (object, optional): Infoblox DiffSync. Defaults to None.
             conn (object): InfobloxAPI connection.
+            config (object): Infoblox config object.
         """
         super().__init__(*args, **kwargs)
         self.job = job
         self.sync = sync
         self.conn = conn
+        self.config = config
+        self.excluded_attrs = config.cf_fields_ignore.get("extensible_attributes", [])
         self.subnets = []
 
         if self.conn in [None, False]:
@@ -55,19 +63,28 @@ class InfobloxAdapter(DiffSync):
             )
             raise PluginImproperlyConfigured
 
-    def load_network_views(self, sync_filters):
-        """Load Namespace DiffSync model."""
+    def load_network_views(self, sync_filters: dict):
+        """Load Namespace DiffSync model.
+
+        Args:
+            sync_filter (dict): Sync filter containing sync rules
+        """
         network_view_filters = {sf["network_view"] for sf in sync_filters if "network_view" in sf}
-        networkviews = self.conn.get_network_views()
-        default_ext_attrs = get_default_ext_attrs(review_list=networkviews)
-        # TODO: Remove after development is done @progala
-        self.job.logger.info(f"NVFilters: {network_view_filters}, NetworkViews: {networkviews}")
+        try:
+            networkviews = self.conn.get_network_views()
+        except requests.exceptions.HTTPError as err:
+            self.job.logger.error(f"Error while loading network views: {str(err)}")
+            raise AdapterLoadException(str(err)) from err
+
+        default_ext_attrs = get_default_ext_attrs(review_list=networkviews, excluded_attrs=self.excluded_attrs)
         for _nv in networkviews:
             # Do not load Network Views not present in the sync filters
             if _nv["name"] not in network_view_filters:
                 continue
-            namespace_name = map_network_view_to_namespace(_nv["name"])
-            networkview_ext_attrs = get_ext_attr_dict(extattrs=_nv.get("extattrs", {}))
+            namespace_name = map_network_view_to_namespace(value=_nv["name"], direction="nv_to_ns")
+            networkview_ext_attrs = get_ext_attr_dict(
+                extattrs=_nv.get("extattrs", {}), excluded_attrs=self.excluded_attrs
+            )
             new_namespace = self.namespace(
                 name=namespace_name,
                 ext_attrs={**default_ext_attrs, **networkview_ext_attrs},
@@ -86,11 +103,8 @@ class InfobloxAdapter(DiffSync):
         """
         containers = []
         subnets = []
-        network_view = None
-        if "network_view" in sync_filter:
-            network_view = sync_filter["network_view"]
-
         prefix_filter_attr = f"prefixes_{ip_version}"
+        network_view = sync_filter["network_view"]
 
         for prefix in sync_filter[prefix_filter_attr]:
             tree = self.conn.get_tree_from_container(root_container=prefix, network_view=network_view)
@@ -104,11 +118,6 @@ class InfobloxAdapter(DiffSync):
                     )
             else:
                 subnets.extend(self.conn.get_all_subnets(prefix=prefix, network_view=network_view))
-
-        # We need to remove duplicate prefixes if Network View support is not enabled
-        if not network_view:
-            containers = self.conn.remove_duplicates(containers)
-            subnets = self.conn.remove_duplicates(subnets)
 
         return containers, subnets
 
@@ -136,7 +145,7 @@ class InfobloxAdapter(DiffSync):
                 containers, subnets = self._load_prefixes_filtered(sync_filter=sync_filter, ip_version="ipv6")
                 all_subnets.extend(subnets)
                 all_containers.extend(containers)
-            # Mimic default behavior of `infoblox_network_view` setting
+            # Load all prefixes from a network view if there are no prefix filters
             if "network_view" in sync_filter and not (pfx_filter_ipv4 or pfx_filter_ipv6):
                 network_view = sync_filter["network_view"]
                 if include_ipv4:
@@ -148,61 +157,24 @@ class InfobloxAdapter(DiffSync):
 
         return all_containers, all_subnets
 
-    def _load_all_prefixes_unfiltered(self, include_ipv4: bool, include_ipv6: bool):
-        """Loads all prefixes from Infoblox. Removes duplicates, if same prefix is found in different network views.
-
-        Args:
-            include_ipv4: Whether to include IPv4 prefixes
-            include_ipv6: Whether to include IPv6 prefixes
-
-        Returns:
-            (tuple): Tuple consisting of list of container prefixes and a list of subnet prefixes
-        """
-        containers = []
-        subnets = []
-        if include_ipv4:
-            containers.extend(self.conn.get_network_containers())
-            subnets.extend(self.conn.get_all_subnets())
-        if include_ipv6:
-            containers.extend(self.conn.get_network_containers(ipv6=True))
-            subnets.extend(self.conn.get_all_subnets(ipv6=True))
-
-        containers = self.conn.remove_duplicates(containers)
-        subnets = self.conn.remove_duplicates(subnets)
-
-        return containers, subnets
-
     def load_prefixes(self, include_ipv4: bool, include_ipv6: bool, sync_filters: Optional[list] = None):
         """Load InfobloxNetwork DiffSync model."""
-        # TODO: Need to align it with the new filter configuration, @progala
-        legacy_sync_filter = {}
-        if PLUGIN_CFG["NAUTOBOT_INFOBLOX_NETWORK_VIEW"]:
-            legacy_sync_filter["network_view"] = PLUGIN_CFG["NAUTOBOT_INFOBLOX_NETWORK_VIEW"]
-        if PLUGIN_CFG["infoblox_import_subnets"]:
-            legacy_sync_filter["prefixes_ipv4"] = PLUGIN_CFG["infoblox_import_subnets"]
-        # TODO: Validate there's no overlap between legacy_sync_filters and sync_filter
-        # Alternatively, refuse to accept sync_filters if old flags are in place @progala
-        sync_filters = PLUGIN_CFG["infoblox_sync_filters"]
-        # TODO: Remove after development is done @progala
-        self.job.logger.info(f"sync_filters: {sync_filters}")
-
-        if not sync_filters:
-            containers, subnets = self._load_all_prefixes_unfiltered(
-                include_ipv4=include_ipv4, include_ipv6=include_ipv6
-            )
-        elif sync_filters:
+        try:
             containers, subnets = self._load_all_prefixes_filtered(
                 sync_filters, include_ipv4=include_ipv4, include_ipv6=include_ipv6
             )
+        except requests.exceptions.HTTPError as err:
+            self.job.logger.error(f"Error while loading prefixes: {str(err)}")
+            raise AdapterLoadException(str(err)) from err
 
         all_networks = containers + subnets
         self.subnets = [(x["network"], x["network_view"]) for x in subnets]
-        default_ext_attrs = get_default_ext_attrs(review_list=all_networks)
+        default_ext_attrs = get_default_ext_attrs(review_list=all_networks, excluded_attrs=self.excluded_attrs)
         for _pf in all_networks:
-            pf_ext_attrs = get_ext_attr_dict(extattrs=_pf.get("extattrs", {}))
+            pf_ext_attrs = get_ext_attr_dict(extattrs=_pf.get("extattrs", {}), excluded_attrs=self.excluded_attrs)
             new_pf = self.prefix(
                 network=_pf["network"],
-                namespace=map_network_view_to_namespace(_pf["network_view"]),
+                namespace=map_network_view_to_namespace(value=_pf["network_view"], direction="nv_to_ns"),
                 description=_pf.get("comment", ""),
                 network_type="network" if _pf in subnets else "container",
                 ext_attrs={**default_ext_attrs, **pf_ext_attrs},
@@ -214,25 +186,27 @@ class InfobloxAdapter(DiffSync):
             try:
                 self.add(new_pf)
             except ObjectAlreadyExists:
-                self.job.logger.warning(
-                    f"Duplicate prefix found: {new_pf}. Duplicate prefixes are not supported, "
-                    "and only the first occurrence will be included in the sync. To load data "
-                    "from a single Network View, use the 'infoblox_network_view' setting."
-                )
+                self.job.logger.warning(f"Duplicate prefix found: {new_pf}.")
 
     def load_ipaddresses(self):
         """Load InfobloxIPAddress DiffSync model."""
         if self.job.debug:
             self.job.logger.debug("Loading IP addresses from Infoblox.")
-        ipaddrs = self.conn.get_all_ipv4address_networks(prefixes=self.subnets)
-        default_ext_attrs = get_default_ext_attrs(review_list=ipaddrs)
+        try:
+            ipaddrs = self.conn.get_all_ipv4address_networks(prefixes=self.subnets)
+        except requests.exceptions.HTTPError as err:
+            self.job.logger.error(f"Error while loading IP addresses: {str(err)}")
+            raise AdapterLoadException(str(err)) from err
+
+        default_ext_attrs = get_default_ext_attrs(review_list=ipaddrs, excluded_attrs=self.excluded_attrs)
         for _ip in ipaddrs:
             _, prefix_length = _ip["network"].split("/")
             dns_name = ""
             if _ip["names"]:
                 dns_name = get_dns_name(possible_fqdn=_ip["names"][0])
-            namespace = map_network_view_to_namespace(_ip["network_view"])
-            ip_ext_attrs = get_ext_attr_dict(extattrs=_ip.get("extattrs", {}))
+            namespace = map_network_view_to_namespace(value=_ip["network_view"], direction="nv_to_ns")
+
+            ip_ext_attrs = get_ext_attr_dict(extattrs=_ip.get("extattrs", {}), excluded_attrs=self.excluded_attrs)
             new_ip = self.ipaddress(
                 address=_ip["ip_address"],
                 prefix=_ip["network"],
@@ -244,19 +218,33 @@ class InfobloxAdapter(DiffSync):
                 description=_ip["comment"],
                 ext_attrs={**default_ext_attrs, **ip_ext_attrs},
             )
-            if not loaded:
-                self.job.logger.warning(
-                    f"Duplicate IP Address {_ip['ip_address']}/{prefix_length} in {_ip['network']} attempting to be loaded."
-                )
+
+            # Record references to DNS Records linked to this IP Address
+            for ref in _ip["objects"]:
+                obj_type = ref.split("/")[0]
+                if obj_type == "record:host":
+                    new_ip.has_host_record = True
+                    new_ip.host_record_ref = ref
+                elif obj_type == "record:a":
+                    new_ip.has_a_record = True
+                    new_ip.a_record_ref = ref
+                elif obj_type == "record:ptr":
+                    new_ip.has_ptr_record = True
+                    new_ip.ptr_record_ref = ref
+
+            self.add(new_ip)
 
     def load_vlanviews(self):
         """Load InfobloxVLANView DiffSync model."""
-        if self.job.debug:
-            self.job.logger.debug("Loading VLAN Views from Infoblox.")
-        vlanviews = self.conn.get_vlanviews()
-        default_ext_attrs = get_default_ext_attrs(review_list=vlanviews)
+        try:
+            vlanviews = self.conn.get_vlanviews()
+        except requests.exceptions.HTTPError as err:
+            self.job.logger.error(f"Error while loading VLAN views: {str(err)}")
+            raise AdapterLoadException(str(err)) from err
+
+        default_ext_attrs = get_default_ext_attrs(review_list=vlanviews, excluded_attrs=self.excluded_attrs)
         for _vv in vlanviews:
-            vv_ext_attrs = get_ext_attr_dict(extattrs=_vv.get("extattrs", {}))
+            vv_ext_attrs = get_ext_attr_dict(extattrs=_vv.get("extattrs", {}), excluded_attrs=self.excluded_attrs)
             new_vv = self.vlangroup(
                 name=_vv["name"],
                 description=_vv["comment"] if _vv.get("comment") else "",
@@ -268,10 +256,15 @@ class InfobloxAdapter(DiffSync):
         """Load InfobloxVlan DiffSync model."""
         if self.job.debug:
             self.job.logger.debug("Loading VLANs from Infoblox.")
-        vlans = self.conn.get_vlans()
-        default_ext_attrs = get_default_ext_attrs(review_list=vlans)
+        try:
+            vlans = self.conn.get_vlans()
+        except requests.exceptions.HTTPError as err:
+            self.job.logger.error(f"Error while loading VLANs: {str(err)}")
+            raise AdapterLoadException(str(err)) from err
+
+        default_ext_attrs = get_default_ext_attrs(review_list=vlans, excluded_attrs=self.excluded_attrs)
         for _vlan in vlans:
-            vlan_ext_attrs = get_ext_attr_dict(extattrs=_vlan.get("extattrs", {}))
+            vlan_ext_attrs = get_ext_attr_dict(extattrs=_vlan.get("extattrs", {}), excluded_attrs=self.excluded_attrs)
             vlan_group = re.search(r"(?:.+\:)(\S+)(?:\/\S+\/.+)", _vlan["_ref"])
             new_vlan = self.vlan(
                 name=_vlan["name"],
@@ -285,28 +278,20 @@ class InfobloxAdapter(DiffSync):
 
     def load(self):
         """Load all models by calling other methods."""
-        # Set ipv4 import to True as default
-        include_ipv4 = True
-        sync_filters = PLUGIN_CFG["infoblox_sync_filters"]
-        if "infoblox_import_objects" in PLUGIN_CFG:
-            # Use config setting to decide whether to import ipv6
-            include_ipv6 = PLUGIN_CFG["infoblox_import_objects"].get("subnets_ipv6")
-            self.load_network_views(include_ipv4=include_ipv4, include_ipv6=include_ipv6, sync_filters=sync_filters)
-            if PLUGIN_CFG["infoblox_import_objects"].get("subnets"):
-                self.load_prefixes(sync_filters=sync_filters)
-            if PLUGIN_CFG["infoblox_import_objects"].get("ip_addresses"):
-                self.load_ipaddresses()
-            if PLUGIN_CFG["infoblox_import_objects"].get("vlan_views"):
-                self.load_vlanviews()
-            if PLUGIN_CFG["infoblox_import_objects"].get("vlans"):
-                self.load_vlans()
-        else:
-            self.job.logger.info("The `infoblox_import_objects` setting was not found so all objects will be imported.")
-            self.load_prefixes()
+        include_ipv4 = self.config.import_ipv4
+        include_ipv6 = self.config.import_ipv4
+        sync_filters = self.config.infoblox_sync_filters
+
+        self.load_network_views(sync_filters=sync_filters)
+        if self.config.import_subnets:
+            self.load_prefixes(include_ipv4=include_ipv4, include_ipv6=include_ipv6, sync_filters=sync_filters)
+        if self.config.import_ip_addresses:
             self.load_ipaddresses()
+        if self.config.import_vlan_views:
             self.load_vlanviews()
+        if self.config.import_vlans:
             self.load_vlans()
-        for obj in ["prefix", "ipaddress", "vlangroup", "vlan", "namespace"]:
+        for obj in ["namespace", "prefix", "ipaddress", "vlangroup", "vlan"]:
             if obj in self.dict():
                 self.job.logger.info(f"Loaded {len(self.dict()[obj])} {obj} from Infoblox.")
 
