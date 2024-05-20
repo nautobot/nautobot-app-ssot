7c19250f42da6eb6e3c77cecbe506b8a5446c420
Przemek Rogala <progala@progala.net>
2024-05-20 19:02:14 +0100

    Remove unused import.

--- nautobot_ssot/tests/infoblox/test_nautobot_adapter.py
+++ nautobot_ssot/tests/infoblox/test_nautobot_adapter.py
@@ -3,7 +3,7 @@
 from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
 
-from nautobot.extras.models import Relationship, RelationshipAssociation, Status
+from nautobot.extras.models import RelationshipAssociation, Status
 from nautobot.ipam.models import Namespace, Prefix, VLAN, VLANGroup
 
 from nautobot_ssot.integrations.infoblox.diffsync.adapters.nautobot import NautobotAdapter
