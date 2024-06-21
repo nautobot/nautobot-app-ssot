7c3e55abf0ffad9cf997037bb0e994da89847d23
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2024-06-21 10:37:14 -0500

    build: ⬆️ Make Nautobot 2.2.0 minimum supported version.

--- nautobot_ssot/__init__.py
+++ nautobot_ssot/__init__.py
@@ -69,7 +69,7 @@ class NautobotSSOTAppConfig(NautobotAppConfig):
     description = "Nautobot app that enables Single Source of Truth.  Allows users to aggregate distributed data sources and/or distribute Nautobot data to other data sources such as databases and SDN controllers."
     base_url = "ssot"
     required_settings = []
-    min_version = "2.0.0"
+    min_version = "2.2.0"
     max_version = "2.9999"
     default_settings = {
         "aci_apics": [],
