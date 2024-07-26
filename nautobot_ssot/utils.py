1127766f4bda2e539d0b26df53843e95952411f1
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2024-07-26 17:15:49 -0500

    refactor: ♻️ Move code to get username/password from HTTP SecretsGroup to utility function.
    This required moving logging import around for integration utils and the App config portion due to errors around Apps not installed yet.

--- nautobot_ssot/utils.py
+++ nautobot_ssot/utils.py
@@ -2,4 +2,25 @@
 
 import logging
 
+from nautobot.extras.choices import SecretsGroupAccessTypeChoices, SecretsGroupSecretTypeChoices
+from nautobot.extras.models import SecretsGroup
+
+
 logger = logging.getLogger("nautobot.ssot")
+
+
+def get_username_password_https_from_secretsgroup(group: SecretsGroup):
+    """Retrieve username and password from a HTTPS SecretsGroup.
+
+    Args:
+        group (SecretsGroup): The SecretsGroup containing HTTPS access-types.
+    """
+    username = group.get_secret_value(
+        access_type=SecretsGroupAccessTypeChoices.TYPE_HTTP,
+        secret_type=SecretsGroupSecretTypeChoices.TYPE_USERNAME,
+    )
+    password = group.get_secret_value(
+        access_type=SecretsGroupAccessTypeChoices.TYPE_HTTP,
+        secret_type=SecretsGroupSecretTypeChoices.TYPE_PASSWORD,
+    )
+    return username, password
