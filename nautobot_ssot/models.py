f573b71cfe3a375bf33bd4ac353d54ac2386dec1
Andrew Turner <aj.turner@live.com>
2024-07-30 12:18:51 -0500

    add exception to get_target_url

--- nautobot_ssot/models.py
+++ nautobot_ssot/models.py
@@ -150,9 +150,15 @@ class Sync(BaseModel):  # pylint: disable=nb-string-field-blank-null
         """Get the absolute url of the target worker associated with this instance."""
         if self.target == "Nautobot" or not self.job_result:
             return None
+        
+        try:
+            class_path = self.job_result.job_model.class_path
+        except AttributeError:
+            return None
+
         return reverse(
             "plugins:nautobot_ssot:data_target",
-            kwargs={"class_path": self.job_result.job_model.class_path},
+            kwargs={"class_path": class_path},
         )
 
 
