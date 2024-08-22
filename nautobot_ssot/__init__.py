9d392698eb6c966337d525b70f4bfdd69147fb16
bakebot <info@networktocode.com>
2024-08-22 15:20:30 +0000

    Cookie updated by NetworkToCode Cookie Drift Manager Tool
    Template:
    
    ```
    {
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "dir": "nautobot-app",
        "ref": "nautobot-app-v2.3.0",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-app-ssot.git",
        "path": "/Users/stephenkiely/outputs/nautobot-app-ssot",
        "repository_path": "/Users/stephenkiely/outputs/nautobot-app-ssot",
        "dir": "",
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "@smith-ntc",
            "full_name": "Network to Code, LLC",
            "email": "opensource@networktocode.com",
            "github_org": "nautobot",
            "app_name": "nautobot_ssot",
            "verbose_name": "Single Source of Truth",
            "app_slug": "nautobot-ssot",
            "project_slug": "nautobot-app-ssot",
            "repo_url": "https://github.com/nautobot/nautobot-app-ssot",
            "base_url": "ssot",
            "min_nautobot_version": "2.0.0",
            "max_nautobot_version": "2.9999",
            "camel_name": "NautobotSSOTApp",
            "project_short_description": "Nautobot Single Source of Truth",
            "model_class_name": "None",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/ssot/en/latest",
            "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
            "_output_dir": "/Users/stephenkiely/outputs",
            "_repo_dir": "/Users/stephenkiely/outputs/.cookiecutters/cookiecutter-nautobot-app/nautobot-app",
            "_checkout": "nautobot-app-v2.3.0"
        },
        "base_branch": "develop",
        "remote_name": "origin",
        "pull_request_strategy": "PullRequestStrategy.CREATE",
        "post_actions": [
            "PostAction.BLACK"
        ],
        "baked_commit_ref": "88cb0d71afd4889658b76c64cfb86d5b347e7565",
        "draft": true
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "",
        "output_dir": "./outputs",
        "push": true,
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "template_dir": "nautobot-app",
        "template_ref": "nautobot-app-v2.3.0",
        "pull_request": null,
        "post_action": [],
        "disable_post_actions": false,
        "draft": null
    }
    ```

--- nautobot_ssot/__init__.py
+++ nautobot_ssot/__init__.py
@@ -1,7 +1,6 @@
 """App declaration for nautobot_ssot."""
 
-import logging
-import os
+# Metadata is inherited from Nautobot. If not including Nautobot in the environment, this should be added
 from importlib import metadata
 
 import packaging
