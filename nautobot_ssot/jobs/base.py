9060abb0267a5d61d304463f8e3abc6bfae5fad3
Gary Snider <75227981+gsnider2195@users.noreply.github.com>
2024-08-28 15:26:09 -0700

    pylint

--- nautobot_ssot/jobs/base.py
+++ nautobot_ssot/jobs/base.py
@@ -112,15 +112,15 @@ class DataSyncBaseJob(Job):  # pylint: disable=too-many-instance-attributes
         - self.job_result (as per Job API)
         """
 
-        def format_size(size):
+        def format_size(size):  # pylint: disable=inconsistent-return-statements
             """Format a size in bytes to a human-readable string. Borrowed from stdlib tracemalloc."""
             for unit in ("B", "KiB", "MiB", "GiB", "TiB"):
                 if abs(size) < 100 and unit != "B":
                     # 3 digits (xx.x UNIT)
-                    return "%.1f %s" % (size, unit)
+                    return "%.1f %s" % (size, unit)  # pylint: disable=consider-using-f-string
                 if abs(size) < 10 * 1024 or unit == "TiB":
                     # 4 or 5 digits (xxxx UNIT)
-                    return "%.0f %s" % (size, unit)
+                    return "%.0f %s" % (size, unit)  # pylint: disable=consider-using-f-string
                 size /= 1024
 
         def record_memory_trace(step: str):
