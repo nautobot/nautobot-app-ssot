da5a089c5aba69036011f42e669dc582d2b0c7d2
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2024-12-16 20:53:43 -0600

    test: ✅ Update tests to adjust for refactor of build_dnac_location_map

--- nautobot_ssot/tests/dna_center/fixtures.py
+++ nautobot_ssot/tests/dna_center/fixtures.py
@@ -14,6 +14,9 @@ EXPECTED_DNAC_LOCATION_MAP = load_json(path="./nautobot_ssot/tests/dna_center/fi
 EXPECTED_DNAC_LOCATION_MAP_WO_GLOBAL = load_json(
     path="./nautobot_ssot/tests/dna_center/fixtures/expected_dnac_location_map_wo_global.json"
 )
+EXPECTED_DNAC_LOCATION_MAP_W_JOB_LOCATION_MAP = load_json(
+    path="./nautobot_ssot/tests/dna_center/fixtures/expected_dnac_location_map_w_job_location_map.json"
+)
 RECV_LOCATION_FIXTURE = load_json(path="./nautobot_ssot/tests/dna_center/fixtures/get_locations_recv.json")
 DEVICE_FIXTURE = load_json(path="./nautobot_ssot/tests/dna_center/fixtures/get_devices.json")
 RECV_DEVICE_FIXTURE = load_json(path="./nautobot_ssot/tests/dna_center/fixtures/get_devices_recv.json")
@@ -22,7 +25,4 @@ RECV_DEVICE_DETAIL_FIXTURE = load_json(path="./nautobot_ssot/tests/dna_center/fi
 PORT_FIXTURE = load_json(path="./nautobot_ssot/tests/dna_center/fixtures/get_port_info.json")
 RECV_PORT_FIXTURE = load_json(path="./nautobot_ssot/tests/dna_center/fixtures/get_port_info_recv.json")
 
-EXPECTED_AREAS = load_json(path="./nautobot_ssot/tests/dna_center/fixtures/expected_areas.json")
-EXPECTED_AREAS_WO_GLOBAL = load_json(path="./nautobot_ssot/tests/dna_center/fixtures/expected_areas_wo_global.json")
-EXPECTED_BUILDINGS = load_json(path="./nautobot_ssot/tests/dna_center/fixtures/expected_buildings.json")
 EXPECTED_FLOORS = load_json(path="./nautobot_ssot/tests/dna_center/fixtures/expected_floors.json")
