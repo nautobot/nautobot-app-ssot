d242f4dfd369bb8ee0e196a1313c60f4435e5ae8
Andrew Turner <aj.turner@live.com>
2024-12-30 12:35:35 -0600

    remove missing import

--- nautobot_ssot/contrib/__init__.py
+++ nautobot_ssot/contrib/__init__.py
@@ -1,6 +1,6 @@
 """SSoT Contrib."""
 
-from nautobot_ssot.contrib.adapter import NautobotAdapter, sort_relationships
+from nautobot_ssot.contrib.adapter import NautobotAdapter
 from nautobot_ssot.contrib.model import NautobotModel
 from nautobot_ssot.contrib.types import (
     CustomFieldAnnotation,
