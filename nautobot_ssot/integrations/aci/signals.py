4d8e14ed138ef4ace3ece111a9f88f5f9304ffd3
Eric Fetty <kingfetty@users.noreply.github.com>
2025-01-15 13:54:09 -0600

    formatted with Ruff

--- nautobot_ssot/integrations/aci/signals.py
+++ nautobot_ssot/integrations/aci/signals.py
@@ -21,6 +21,7 @@ def register_signals(sender):
     nautobot_database_ready.connect(device_custom_fields, sender=sender)
     nautobot_database_ready.connect(interface_custom_fields, sender=sender)
 
+
 def _ensure_tag(apps, name, color):
     """Ensure tag exists and properly configured."""
     tag = apps.get_model("extras", "Tag")
@@ -30,6 +31,7 @@ def _ensure_tag(apps, name, color):
         if content_type not in _tag.content_types.all():
             _tag.content_types.add(content_type)
     _tag.validated_save()
+
 
 def aci_create_tag(apps, **kwargs):
     """Add a tag."""
@@ -44,7 +46,9 @@ def aci_create_tag(apps, **kwargs):
     if apics:
         for key in apics:
             if ("SITE" in key or "STAGE" in key) and not tag.objects.filter(name=apics[key]).exists():
-                _ensure_tag(apps=apps, name=apics[key], color="".join([random.choice("ABCDEF0123456789") for i in range(6)]))  # noqa: S311
+                _ensure_tag(
+                    apps=apps, name=apics[key], color="".join([random.choice("ABCDEF0123456789") for i in range(6)])
+                )  # noqa: S311
 
 
 def aci_create_manufacturer(apps, **kwargs):
