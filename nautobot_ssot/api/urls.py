d5b1064ce41cb9f6ab62b7a6df7666a7ed63baaf
Stephen Kiely <stephenkiely87@gmail.com>
2025-02-28 08:57:35 -0600

    Remove unnecessary changes from Drift Manager

--- nautobot_ssot/api/urls.py
+++ nautobot_ssot/api/urls.py
@@ -4,10 +4,16 @@ from nautobot.apps.api import OrderedDefaultRouter
 
 from nautobot_ssot.integrations.utils import each_enabled_integration_module
 
-app_name = "nautobot_ssot-api"
 urlpatterns = []
 router = OrderedDefaultRouter()
-# add the name of your api endpoint, usually hyphenated model name in plural, e.g. "my-model-classes"
+
+
+def _add_integrations():
+    for module in each_enabled_integration_module("api.urls"):
+        urlpatterns.extend(module.urlpatterns)
+
+
+_add_integrations()
 
 app_name = "nautobot_ssot-api"
-urlpatterns = router.urls
+urlpatterns += router.urls
