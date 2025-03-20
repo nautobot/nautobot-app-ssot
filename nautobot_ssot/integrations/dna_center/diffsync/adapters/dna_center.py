d037615f12c100a238861eec2d28426688444f6f
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2025-03-20 14:16:45 -0500

    fix: Tweak list comprehension and format for ruff

--- nautobot_ssot/integrations/dna_center/diffsync/adapters/dna_center.py
+++ nautobot_ssot/integrations/dna_center/diffsync/adapters/dna_center.py
@@ -348,9 +348,11 @@ class DnaCenterAdapter(Adapter):
                 # remove Global if not importing Global
                 if not settings.PLUGINS_CONFIG["nautobot_ssot"].get("dna_center_import_global"):
                     locations.pop(0)
-                loc_found = [if loc in self.dnac_location_map for loc in locations]
+                loc_found = [loc in self.dnac_location_map for loc in locations]
                 if not all(loc_found):
-                    self.job.logger.error(f"Device {dev['hostname']} has unknown location {loc} so will not be imported.")
+                    self.job.logger.error(
+                        f"Device {dev['hostname']} has unknown location in hierarchy so will not be imported."
+                    )
                     dev["field_validation"] = {
                         "reason": "Invalid location information found.",
                         "device_details": dev_details,
