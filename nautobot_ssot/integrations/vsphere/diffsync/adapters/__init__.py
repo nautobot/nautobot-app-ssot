qduk/nautobot-plugin-ssot

Got first test file working

nautobot_ssot/integrations/vsphere/diffsync/adapters/__init__.py
@@ -1,0 +1,6 @@
+"""Collection of adapters."""
+
+from .adapter_nautobot import Adapter
+from .adapter_vsphere import VsphereDiffSync
+
+__all__ = ("Adapter", "VsphereDiffSync")

nautobot/nautobot-app-ssot

nautobot_ssot/integrations/vsphere/diffsync/adapters/__init__.py
@@ -1,0 +1,6 @@
+"""Collection of adapters."""
+
+from .adapter_nautobot import NBAdapter
+from .adapter_vsphere import VsphereDiffSync
+
+__all__ = ("NBAdapter", "VsphereDiffSync")