5da201cdaf886d90767b0794d5ba6bf78f2b1d0c
Andrew Turner <aj.turner@live.com>
2025-04-17 14:18:12 -0500

    updates

--- nautobot_ssot/integrations/cradlepoint/jobs.py
+++ nautobot_ssot/integrations/cradlepoint/jobs.py
@@ -13,7 +13,7 @@ from nautobot.extras.choices import (
     SecretsGroupAccessTypeChoices,
     SecretsGroupSecretTypeChoices,
 )
-
+from nautobot_ssot.integrations.cradlepoint.models import SSOTCradlepointConfig
 from nautobot_ssot.integrations.cradlepoint.constants import DEFAULT_API_DEVICE_LIMIT
 from nautobot_ssot.integrations.cradlepoint.diffsync.adapters.cradlepoint import CradlepointSourceAdapter
 from nautobot_ssot.integrations.cradlepoint.diffsync.adapters.nautobot import NautobotTargetAdapter
@@ -22,45 +22,25 @@ from nautobot_ssot.integrations.cradlepoint.utilities.clients import (
     CradlepointClient,
 )
 from nautobot_ssot.jobs.base import DataMapping, DataSource
+from nautobot_ssot.integrations.cradlepoint.utilities.clients import cradlepoint_client
 
 name = "SSoT - Cradlepoint"  # pylint: disable=invalid-name
-
-
-def get_cradlepoint_client_config(app_config, debug):
-    """Get Cradlepoint client config from the Cradlepoint config instance."""
-    x_ecm_api_id = app_config.cradlepoint_instance.secrets_group.get_secret_value(
-        access_type=SecretsGroupAccessTypeChoices.TYPE_REST,
-        secret_type=SecretsGroupSecretTypeChoices.TYPE_USERNAME,
-    )
-    x_ecm_api_key = app_config.cradlepoint_instance.secrets_group.get_secret_value(
-        access_type=SecretsGroupAccessTypeChoices.TYPE_REST,
-        secret_type=SecretsGroupSecretTypeChoices.TYPE_PASSWORD,
-    )
-    x_cp_api_id = app_config.cradlepoint_instance.secrets_group.get_secret_value(
-        access_type=SecretsGroupAccessTypeChoices.TYPE_REST,
-        secret_type=SecretsGroupSecretTypeChoices.TYPE_SECRET,
-    )
-    x_cp_api_key = app_config.cradlepoint_instance.secrets_group.get_secret_value(
-        access_type=SecretsGroupAccessTypeChoices.TYPE_REST,
-        secret_type=SecretsGroupSecretTypeChoices.TYPE_TOKEN,
-    )
-    return {
-        "base_url": app_config.cradlepoint_instance.remote_url,
-        "x_ecm_api_id": x_ecm_api_id.strip(),
-        "x_ecm_api_key": x_ecm_api_key.strip(),
-        "x_cp_api_id": x_cp_api_id.strip(),
-        "x_cp_api_key": x_cp_api_key.strip(),
-        "verify_ssl": app_config.cradlepoint_instance.verify_ssl,
-        "debug": debug,
-        "page_limit": 100,
-    }
 
 
 # pylint:disable=too-few-public-methods
 class CradlepointDataSource(DataSource):  # pylint: disable=too-many-instance-attributes
     """Cradlepoint SSoT Data Source."""
 
+    class Meta:
+        """Metadata about this Job."""
+
+        name = "Cradlepoint ⟹ Nautobot"
+        data_source = "Cradlepoint"
+        data_source_icon = static("nautobot_ssot_cradlepoint/cradlepoint_logo.png")
+        description = "Sync data from Cradlepoint into Nautobot."
+
     debug = BooleanVar(description="Enable for more verbose debug logging")
+    
     config = ObjectVar(
         model=SSOTCradlepointConfig,
         display_field="SSOT Cradlepoint Config",
@@ -79,16 +59,10 @@ class CradlepointDataSource(DataSource):  # pylint: disable=too-many-instance-at
     def __init__(self):
         """Initialize CradlepointDataSource."""
         super().__init__()
+
+        # TODO: Change to configurable option in job
         self.diffsync_flags = DiffSyncFlags.CONTINUE_ON_FAILURE
-
-    class Meta:
-        """Metadata about this Job."""
-
-        name = "Cradlepoint ⟹ Nautobot"
-        data_source = "Cradlepoint"
-        data_source_icon = static("nautobot_ssot_cradlepoint/cradlepoint_logo.png")
-        description = "Sync data from Cradlepoint into Nautobot."
-
+    
     @classmethod
     def config_information(cls):
         """Dictionary describing the configuration of this DataSource."""
@@ -106,22 +80,12 @@ class CradlepointDataSource(DataSource):  # pylint: disable=too-many-instance-at
             ),
         )
 
-    def run(
-        self,
-        dryrun,
-        config,
-        starting_offset,
-        memory_profiling,
-        debug,
-        *args,
-        **kwargs,
-    ):  # pylint: disable=arguments-differ, too-many-arguments
+    def run(self, dryrun, config, memory_profiling, debug, *args, **kwargs):
         """Run sync."""
         self.dryrun = dryrun
         self.debug = debug
         self.memory_profiling = memory_profiling
         self.config = config
-        self.starting_offset = starting_offset
         super().run(dryrun, memory_profiling, *args, **kwargs)
 
     def load_source_adapter(self):
@@ -130,11 +94,8 @@ class CradlepointDataSource(DataSource):  # pylint: disable=too-many-instance-at
         self.source_adapter = CradlepointSourceAdapter(
             job=self,
             sync=self.sync,
-            client=CradlepointClient(
-                **get_cradlepoint_client_config(self.config, self.debug)
-            ),
+            client=cradlepoint_client(self.config, self.debug),
             config=self.config,
-            starting_offset=self.starting_offset,
         )
         self.logger.info("Loading data from Cradlepoint...")
         self.source_adapter.load()
