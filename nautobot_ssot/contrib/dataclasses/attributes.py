043bad6f4f5ce9f317629c3e7b7cd53aad73af4b
Andrew Turner <aj.turner@live.com>
2025-06-02 11:24:56 -0500

    add files

---
+++ nautobot_ssot/contrib/dataclasses/attributes.py
@@ -0,0 +1,126 @@
+"""Data classes for interacting with attributes in the Nautobot database."""
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+from django.db.models import Model
+from typing_extensions import Type, List
+from django.contrib.contenttypes.models import ContentType
+
+
+
+@dataclass
+class AttributeInterface(ABC):
+    """Interface class for getting attribute-specific data from Nautobot.
+    
+    The contrib modules interact with Nautobot objects to get attribute information. The process
+    of getting that data can vary between attribute types and each has their own set of requirements.
+
+    This interface class for attributes provides base requirements for child classes to implement. Any
+    external interactions with implementations should be done using methods defined in this interface
+    class. Any child classes should not have dependencies for interactions based on added methods or
+    attributes.
+    """
+
+    name: str
+    model_class: Type[Model]
+    type_hints: dict = field(repr=False)
+    is_sortable: bool = field(default=False)
+    metadata: list = field(init=False, repr=False)
+
+    def __post_init__(self):
+        """Post initialization."""
+        self.__init_attr__()
+
+    def __init_attr__(self):
+        """Placeholder method called by `__post_init__` to avoid overloading it."""
+
+    @abstractmethod
+    def load(self, obj: Model):
+        """Abstract method for loading attribute value.
+
+        This method is specific to each implementation as different implementations do not the same
+        process or requirements for loading attribute data.
+        """
+
+
+@dataclass
+class StandardAttribute(AttributeInterface):
+    """Standard attribute interface.
+
+    Standard attributes are native attributes for the specific model and represented as columns within the database.
+    """
+
+    def load(self, obj: Model):
+        """Standard attributes return the value stored within the model, no additional processing required."""
+        return getattr(obj, self.name)
+
+
+@dataclass
+class ForeignKeyAttribute(AttributeInterface):
+    """Attribute interface for foreign keys."""
+    
+    lookups: List[str] = field(init=False, repr=False)
+    related_attr_name: str = field(init=False, repr=False)
+
+    def __init_attr__(self):
+        """Initialize the attribute."""
+        if "__" not in self.name:
+            raise ValueError(f"Foreign key values require double underscores in attribute name, got {self.name}")
+        self.lookups = self.name.split("__")
+        self.related_attr_name = self.lookups.pop(-1)
+
+    def get_related_object(self, obj: Model, attribute: str):
+        """Get related object from Django model instance."""
+        return getattr(obj, attribute)
+    
+    def get_nested_related_object(self, obj: Model):
+        """Get related objects from the database, multiple relations deep."""
+        # Need to get initial related object before loop
+        # NOTE: Can't use .pop() because we need the list intact for multiple calls.
+        if related_object := getattr(obj, self.lookups[0]):
+            # Ignore first entry and last entry in loop
+            for lookup in self.lookups[1:-1]:
+                related_object = self.get_related_object(related_object, lookup)
+                if not related_object:
+                    break
+            else:
+                return related_object
+        return None
+    
+    def get_lookup_value(self, obj: Model):
+        """Get the value for an attribute of a related object by its lookup."""
+        attr_name = self.lookups[-1]
+        try:
+            return getattr(obj, attr_name)
+        # If the lookup doesn't point anywhere, check whether it is using the convention for generic foreign keys.
+        except AttributeError:
+            if attr_name in ["app_label", "model"]:
+                return getattr(ContentType.objects.get_for_model(obj), attr_name)
+        return None
+
+    def load(self, obj: Model):
+        """Load the foreign key value."""
+        related_object = self.get_nested_related_object(obj)
+        # If the foreign key does not point to anything, return None
+        if not related_object:
+            return None
+        # Return the result of the last lookup directly.
+        if attr_value := self.get_lookup_value(related_object):
+            return attr_value
+        return None
+
+
+@dataclass
+class CustomForeignKeyAttribute(AttributeInterface):
+    """"""
+
+
+
+@dataclass
+class ManyRelationshipAttribute(AttributeInterface):
+    """"""
+
+
+@dataclass
+class CustomManyRelationshipAttribute(AttributeInterface):
+    """"""
---
+++ nautobot_ssot/tests/contrib/dataclasses/test_attributes.py
@@ -0,0 +1,159 @@
+from django.test import TestCase, TransactionTestCase
+
+from nautobot.dcim.models import LocationType, Location
+from nautobot_ssot.contrib.dataclasses.attributes import (
+    StandardAttribute,
+    ForeignKeyAttribute,
+)
+from diffsync import DiffSyncModel
+from typing_extensions import Optional, get_type_hints
+from unittest.mock import MagicMock
+from nautobot.extras.models import Status
+
+
+
+class LocationModel(DiffSyncModel):
+    """"""
+
+    _model = Location
+    _modelname = "location"
+    _identifiers = (
+        "name",
+        "parent__name",
+        "parent__location_type__name",
+    )
+    _attributes = ("description", "latitude",)
+
+    name: str
+    parent__name: str
+    parent__location_type__name: str
+    description: Optional[str] = ""
+    latitude: Optional[int] = ""
+
+
+class BaseTestCase(TestCase):
+    """"""
+
+    def _load_location_types(self):
+        self.location_type_1 = LocationType(
+            name="Test Location Type",
+        )
+        self.location_type_1.save()
+
+    def _load_locations(self):
+        status = Status.objects.get(name="Active")
+        self.location_1 = Location(
+            name="Location 1",
+            location_type=self.location_type_1,
+            status=status,
+        )
+        self.location_1.save()
+
+        status = Status.objects.get(name="Active")
+        self.location_2 = Location(
+            name="Location 2",
+            location_type=self.location_type_1,
+            parent=self.location_1,
+            status=status,
+        )
+        self.location_2.save()
+
+    def setUp(self):
+        """Setup the test cases."""
+        self._load_location_types()
+        self._load_locations()
+        self.model_type_hints = get_type_hints(LocationModel, include_extras=True)
+
+
+class TestStandardAttribute(BaseTestCase):
+    """Test cases for standard attributes."""
+
+    def setUp(self):
+        """"""
+        super().setUp()
+        self.name = StandardAttribute(
+            name="name",
+            model_class=Location,
+            type_hints=self.model_type_hints["name"],
+        )
+        self.description = StandardAttribute(
+            name="description",
+            model_class=Location,
+            type_hints=self.model_type_hints["description"],
+        )
+        self.latitude = StandardAttribute(
+            name="latitude",
+            model_class=Location,
+            type_hints=self.model_type_hints["latitude"],
+        )
+
+    def test_get_standard_attribute(self):
+        """Test loading a standard attribute."""
+        result = self.name.load(self.location_1)
+        self.assertEqual("Location 1", result)
+
+    def test_optional_blank_not_none_attribute(self):
+        """Test loading an optional string attribute without a value, but not None in database.
+        
+        Django validated save for `blank=True` and `null=False` means a validated save will allow an
+        empty string or None value, but the database will not except a null value.
+        """
+        result = self.description.load(self.location_1)
+        self.assertIsNotNone(result)
+        self.assertEqual(result, "")
+
+    def test_optional_value(self):
+        """Test for loading an optional value that can be a null value."""
+        result = self.latitude.load(self.location_1)
+        self.assertIsNone(result)
+
+
+class TestForeignKeyAttribute(BaseTestCase):
+    """"""
+
+    def setUp(self):
+        """"""
+        super().setUp()
+        self.parent_name = ForeignKeyAttribute(
+            "parent__name",
+            model_class=Location,
+            type_hints=self.model_type_hints["parent__name"],
+        )
+        self.parent_location_type_name = ForeignKeyAttribute(
+            "parent__location_type__name",
+            model_class=Location,
+            type_hints=self.model_type_hints["parent__location_type__name"],
+        )
+
+    def test_instantiate(self):
+        """Test to ensure class is properly instantiated."""
+        self.assertEqual(len(self.parent_name.lookups), 1)
+        self.assertEqual(self.parent_name.related_attr_name, "name")
+
+    def test_invalid_attribute_name(self):
+        """"""
+        with self.assertRaises(ValueError):
+            ForeignKeyAttribute(
+                "parent",
+                model_class=Location,
+                type_hints=self.model_type_hints["parent__name"]
+            )
+    
+    def test_empty_foreign_key(self):
+        """Test an unset foreign key."""
+        self.assertIsNone(self.parent_name.load(self.location_1))
+
+    def test_nested_instantiate(self):
+        """Test to make sure a nested foreign key attribute is properly instantiated."""
+        self.assertAlmostEqual(len(self.parent_location_type_name.lookups), 2)
+        self.assertEqual(self.parent_location_type_name.related_attr_name, "name")
+
+    def test_load_valid_foreign_key(self):
+        """"""
+        result = self.parent_name.load(self.location_2)
+        self.assertEqual(result, "Location 1")
+
+    def test_nested_load_valid_foreign_key(self):
+        """"""
+        result = self.parent_location_type_name.load(self.location_2)
+        self.assertEqual(result, "Test Location Type")
