b5118f7b04d94372d7492740636a39085081a467
Zach Biles @bile0026 <zach.biles@networktocode.com>
2025-06-12 07:53:53 -0500

    fix: 🐛 fix tests and signals issues

--- development/nautobot_config.py
+++ development/nautobot_config.py
@@ -187,8 +187,10 @@ PLUGINS_CONFIG = {
             "computed_field": True,
             "tag": True,
             "graph_ql_query": True,
-            "software": True,
-            "software_image": True,
+            "software": True, # for nautobot <2.3.0
+            "software_version": True, # for nautobot >2.3.0
+            "software_image": True, # for nautobot <2.3.0
+            "software_image_file": True, # for nautobot >2.3.0
             "validated_software": True,
             "tenant_group": True,
             "tenant": True,
