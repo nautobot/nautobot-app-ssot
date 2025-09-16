10de22e7f403b2376e09c6dfce110594bc52c01d
bakebot <info@networktocode.com>
2025-09-16 17:31:39 +0000

    Cookie updated by NetworkToCode Cookie Drift Manager Tool
    Template:
    
    ```
    {
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "dir": "nautobot-app",
        "ref": "nautobot-app-v2.6.0",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-app-ssot.git",
        "path": "/tmp/tmpwg62jxuy/nautobot-app-ssot",
        "repository_path": "/tmp/tmpwg62jxuy/nautobot-app-ssot",
        "dir": "",
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "@smith-ntc",
            "full_name": "Network to Code, LLC",
            "email": "opensource@networktocode.com",
            "github_org": "nautobot",
            "app_name": "nautobot_ssot",
            "verbose_name": "Single Source of Truth",
            "app_slug": "nautobot-ssot",
            "project_slug": "nautobot-app-ssot",
            "repo_url": "https://github.com/nautobot/nautobot-app-ssot",
            "base_url": "ssot",
            "camel_name": "NautobotSSOTApp",
            "project_short_description": "Nautobot Single Source of Truth",
            "model_class_name": "Sync",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/ssot/en/latest",
            "_extensions": [
                "local_extensions.camel_case_to_kebab",
                "local_extensions.camel_case_to_words",
                "local_extensions.NautobotVersions"
            ],
            "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
            "_output_dir": "/tmp/tmpwg62jxuy",
            "_repo_dir": "/github/home/.cookiecutters/cookiecutter-nautobot-app/nautobot-app",
            "_checkout": "nautobot-app-v2.6.0"
        },
        "base_branch": "develop",
        "remote_name": "origin",
        "pull_request_strategy": "PullRequestStrategy.UPDATE_OR_CREATE",
        "post_actions": [],
        "baked_commit_ref": "4d0e57e0195ea0cef972e828baf5f72b8d47a617",
        "draft": false
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "",
        "output_dir": "",
        "push": true,
        "template": "",
        "template_dir": "",
        "template_ref": "nautobot-app-v2.6.0",
        "pull_request": "update-or-create",
        "post_action": [],
        "disable_post_actions": true,
        "draft": null
    }
    ```

--- nautobot_ssot/__init__.py
+++ nautobot_ssot/__init__.py
@@ -18,11 +18,10 @@ class NautobotSSOTAppConfig(NautobotAppConfig):
     description = "Nautobot Single Source of Truth."
     base_url = "ssot"
     required_settings = []
-    min_version = "2.0.0"
-    max_version = "2.9999"
     default_settings = {}
     caching_config = {}
     docs_view_name = "plugins:nautobot_ssot:docs"
+    searchable_models = ["sync"]
 
 
 config = NautobotSSOTAppConfig  # pylint:disable=invalid-name
