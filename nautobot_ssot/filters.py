6c2b40c535ae26191e18696b457162137194a9af
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2025-10-15 13:57:54 -0500

    fix: 🐛 Remove invalid filters

--- nautobot_ssot/filters.py
+++ nautobot_ssot/filters.py
@@ -57,14 +57,12 @@ class SyncRecordFilterSet(NameSearchFilterSet, NautobotFilterSet):  # pylint: di
 
     q = SearchFilter(
         filter_predicates={
-            "sync": "icontains",
             "source": "icontains",
             "target": "icontains",
             "obj_type": "icontains",
             "obj_name": "icontains",
             "action": "icontains",
             "status": "icontains",
-            "synced_object_type": "icontains",
         }
     )
 
@@ -82,8 +80,6 @@ class SyncRecordFilterSet(NameSearchFilterSet, NautobotFilterSet):  # pylint: di
             "obj_name",
             "action",
             "status",
-            "synced_object_type",
-            "synced_object_id",
         ]
 
 
