c92d7653850fb20d4908e4a9210a918986d226f1
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2025-10-21 16:32:24 -0500

    fix: 🐛 Remove action keyword from function as not used

--- nautobot_ssot/views.py
+++ nautobot_ssot/views.py
@@ -446,10 +446,8 @@ class SyncRecordUIViewSet(ReadOnlyNautobotUIViewSet):
         ],
     )
 
-
-def process_bulk_syncrecords(request, action="None"):
+def process_bulk_syncrecords(request):
     """Endpoint for processing mulitple SyncRecords."""
-    _action = action
     pks = request.POST.getlist("pk")
     if not pks:
         messages.error(request, "No items selected for bulk action")
