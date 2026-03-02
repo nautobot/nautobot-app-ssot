563b1c2b3b5caf3ed4e74089862f78789be5cc8f
Andrew Turner <aj.turner@live.com>
2026-03-02 09:35:59 -0600

    updates

--- nautobot_ssot/utils/diffsync.py
+++ nautobot_ssot/utils/diffsync.py
@@ -2,7 +2,7 @@
 
 from typing import List
 
-from typing_extensions import get_type_hints, ClassVar, get_args, Union, Type, Any
+from typing import get_type_hints, ClassVar, get_args, Type, Any, get_origin, Tuple, Annotated
 from nautobot_ssot.contrib.types import CustomAnnotation
 from functools import lru_cache
 
@@ -30,29 +30,32 @@ class DiffSyncModelUtilityMixin:
 
     @classmethod
     @lru_cache
+    def get_attr_args(cls, attr_name: str) -> Tuple:
+        return get_args(cls.get_type_hints()[attr_name])
+
+    @classmethod
+    @lru_cache
     def get_attr_annotation(cls, attr_name: str) -> Any:
         """Get custom annotation from attribute metadata, if it exists. Returns None if attribute is missing or not annotated."""
-        type_hints_dict = cls.get_type_hints()
-        type_hints = type_hints_dict.get(attr_name)
-        if type_hints is None:
-            return None
-        for metadata in getattr(type_hints, "__metadata__", []):
+        # Looping through args vs. returning static index ensures getting the annotation in the instance it's not in the [1] position.
+        for metadata in cls.get_attr_args(attr_name):
             if isinstance(metadata, CustomAnnotation):
                 return metadata
         return None
 
     @classmethod
     @lru_cache
-    def get_attr_type(cls, attr_name: str) -> Union[Type, None]:
-        """Get attribute class type from attributes with annotated. Returns None if attribute is missing or not typed."""
-        type_hints_dict = get_type_hints(cls, include_extras=True)
-        type_hint = type_hints_dict.get(attr_name)
-        if type_hint is None:
-            return None
-        try:
-            return get_args(type_hint)[0]
-        except (IndexError, TypeError):
-            # Not an annotated type or no args
-            if isinstance(type_hint, type):
-                return type_hint
-            return None
+    def is_attr_annotated(cls, attr_name: str) -> bool:
+        """"""
+        return get_origin(cls.get_type_hints()[attr_name]) in [Annotated]
+
+    @classmethod
+    @lru_cache
+    def get_attr_type(cls, attr_name: str) -> Type:
+        """Get class type of specified attribute.
+
+        NOTE: If attribute is `Annotated`, we return the inner type, not `Annotated`.
+        """
+        if cls.is_attr_annotated(attr_name):
+            return cls.get_attr_args(attr_name)[0]
+        return cls.get_type_hints()[attr_name]
