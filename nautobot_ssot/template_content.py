54beb2081b5d86c5d2790ee2952f88f6fc4b6e8a
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2026-03-16 17:05:01 -0500

    fix: 🐛 Fix bug in Template when rendering Sync object.

--- nautobot_ssot/template_content.py
+++ nautobot_ssot/template_content.py
@@ -21,17 +21,18 @@ class JobResultSyncLink(TemplateExtension):
             return ""
         if sync_objects.count() == 0:
             return ""
+        sync = sync_objects.first()
         if sync_objects.count() > 1:
             return f"""
                 <div class="btn-group">
-                    <a href="{reverse('plugins:nautobot_ssot:sync', kwargs={'pk': sync_objects.pk})}" class="btn btn-primary">
-                        <span class="mdi mdi-database-sync-outline"></span> SSoT Sync Details OF FIRST RECORD, THERE WERE MULTIPLE FOUND
+                    <a href="{reverse('plugins:nautobot_ssot:sync', kwargs={'pk': sync.pk})}" class="btn btn-primary">
+                        <span class="mdi mdi-database-sync-outline"></span> SSoT Sync Details (first of multiple)
                     </a>
                 </div>
             """
         return f"""
             <div class="btn-group">
-                <a href="{reverse('plugins:nautobot_ssot:sync', kwargs={'pk': sync_objects.pk})}" class="btn btn-primary">
+                <a href="{reverse('plugins:nautobot_ssot:sync', kwargs={'pk': sync.pk})}" class="btn btn-primary">
                     <span class="mdi mdi-database-sync-outline"></span> SSoT Sync Details
                 </a>
             </div>
